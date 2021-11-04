3323155b7abf1fe4ac88d2ff20a29b9f93c4147a
N0I0C0K <nick131410@aliyun.com>
2021-11-04 23:18:56 +0800

    更新了背景

--- src/buttom-page.jsx
+++ src/buttom-page.jsx
@@ -5,15 +5,13 @@ import ReactDOM from 'react-dom'
 class TopButtom extends React.Component {
     render() {
         return (
-            <a href="" class="min-h-full m-4 gap-40">{this.props.name}</a>
+            <a href="" class="min-h-full mt-4 gap-40 text-lg w-20">{this.props.name}</a>
         )
     }
 }
 
 ReactDOM.render(
-    <div class="p-3 flex justify-end space-x-4 min-h-full rounded-l shadow-lg object-center 
-    bg-red-100
-    sticky">
+    <div class="pr-5 flex justify-end space-x-4 min-h-full rounded-lg shadow-lg object-center w-11/12 bg-red-100 backdrop-blur-xl backdrop-brightness-95">
         <TopButtom name="首页" />
         <TopButtom name="关于我们" />
     </div>,
